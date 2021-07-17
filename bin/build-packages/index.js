4376c802c18707b5750d55ec7b0c0807c72dbf2d
Alain Armand <idkjs@users.noreply.github.com>
2021-07-17 19:24:19 +0200

    graphql not building:

---
+++ bin/build-packages/index.js
@@ -0,0 +1,31 @@
+const fs = require("fs-extra");
+const { exec } = require("child_process");
+const { log, logGreen, logRed } = require("../utils");
+
+build();
+
+async function build() {
+  let dir = `${__dirname}/../../packages`;
+
+  try {
+    let packages = await fs.readdir(dir);
+    packages.map((pkg) => {
+      log(`Started building ${pkg}`);
+      exec(
+        `cd ${__dirname}/../../packages/${pkg} && npm i --legacy-peer-deps && npm run build`,
+        (err, stdout, stderr) => {
+          if (err) {
+            logRed(`Finished building ${pkg} with errors...`);
+            console.error(err);
+          } else {
+            console.log(stdout);
+            console.log(stderr);
+            logGreen(`Successfully finished building ${pkg}`);
+          }
+        }
+      );
+    });
+  } catch (_) {
+    log("`packages` directory not found!");
+  }
+}
